8f019900ba692181c1f23f04245fe160496b5f4b
niadul-munna <niadulislammunna@gmail.com>
2025-05-24 23:27:01 +0600

    Refactor : Add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useReducer } from "react";
+import MovieList from "./components/cine/MovieList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { MovieContext } from "./context";
+import { cartReducer, initialState } from "./reducers/cartReducer";
+import { ToastContainer } from "react-toastify";
+
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-amber-400">
-        Hello world!
-      </h1>
-    </>
+    <MovieContext.Provider value={{ state, dispatch }}>
+      <Header />
+      <main>
+        <div className="container mx-auto grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </MovieContext.Provider>
   );
 }
 
